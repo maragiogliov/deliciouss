d2ebbd88a0123dbb95046005e85dd2b04818cf17
nonexistentdodo <nonexistentdodo@protonmail.com>
2022-04-05 18:14:48 +0100

    cache-out

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import Veggies from "../components/Veggies";
+import Popular from "../components/Popular";
+import {motion} from 'framer-motion';
+
+import React from 'react'
+
+function Home() {
+  return (
+    <motion.div
+    animate={{opacity:1 }}
+    initial={{opacity: 0 }}
+    exit={{opacity: 0 }}
+    transition={{duration: 0.5 }}
+    >
+        <Veggies />
+        <Popular />
+       
+    </motion.div>
+  )
+}
+
+export default Home
